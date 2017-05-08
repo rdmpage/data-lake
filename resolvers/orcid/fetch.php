2b0e58325a2f419d6b4f0db44cad14caeceb8217
Roderic Page <r.page@bio.gla.ac.uk>
2017-05-08 22:08:37 +0100

    ORCID resolver stores individual works

--- resolvers/orcid/fetch.php
+++ resolvers/orcid/fetch.php
@@ -3,7 +3,11 @@
 // ORCID API
 
 require_once (dirname(dirname(__FILE__)) . '/lib.php');
+require_once (dirname(dirname(__FILE__)) . '/nameparse.php');
+require_once (dirname(dirname(__FILE__)) . '/fingerprint.php');
 require_once (dirname(dirname(__FILE__)) . '/shared/crossref.php');
+
+require_once (dirname(dirname(dirname(__FILE__))) . '/documentstore/couchsimple.php');
 
 
 /*
@@ -19,12 +23,16 @@ require_once (dirname(dirname(__FILE__)) . '/shared/crossref.php');
 //----------------------------------------------------------------------------------------
 function orcid_works($obj, $lookup_works)
 {
+	global $couch;
+	
+	$force = true;
+
 	$links = array();
 	
-	$works = array();
+	// ORCID
+	$orcid = $obj->{'orcid-profile'}->{'orcid-identifier'}->uri;	
 	
-	// Extract works
-	
+	// Extract works and create CSL JSON objects for each one
 	if (isset($obj->{'orcid-profile'}->{'orcid-activities'}))
 	{
 		$works = $obj->{'orcid-profile'}->{'orcid-activities'}->{'orcid-works'}->{'orcid-work'};
@@ -33,32 +41,34 @@ function orcid_works($obj, $lookup_works)
 		{
 			foreach ($works as $work)
 			{
+				$doc = new stdclass;
+				
+				// Use put-code as bnode identifier for this work
+				$doc->_id = $orcid . '-' . $work->{'put-code'};
+				$doc->{'message-format'} = 'application/vnd.crossref-api-message+json';		
+			
+				// Get reference details--------------------------------------------------
 				$reference = new stdclass;
-	
-				// Use put-code as bnode identifier
-				$reference->id = $work->{'put-code'};
-		
 				$reference->title = $work->{'work-title'}->{'title'}->value;
 	
 				// Journal?
 				if (isset($work->{'journal-title'}->value))
 				{
-					$reference->journal = $work->{'journal-title'}->value;
+					$reference->{'container-title'}[] = $work->{'journal-title'}->value;
+					$reference->type = 'journal-article';
 				}		
 		
-				// date
-				$date = '';
 				if (isset($work->{'publication-date'}))
 				{
+					$reference->issued = new stdclass;
+					$reference->issued->{'date-parts'} = array();
 					if (isset($work->{'publication-date'}->{'year'}->value))
 					{
-						$date = $work->{'publication-date'}->{'year'}->value;
+						$reference->issued->{'date-parts'}[] = array($work->{'publication-date'}->{'year'}->value);
 					}
-					$reference->date = $date;
-				}
-		
+				}		
 
-				// Parse BibTex-------------------------------------------------------------------
+				// Parse BibTex-----------------------------------------------------------
 				if (isset($work->{'work-citation'}->citation))
 				{
 					$bibtext = $work->{'work-citation'}->citation;
@@ -67,15 +77,18 @@ function orcid_works($obj, $lookup_works)
 					{
 						if (preg_match('/journal = \{(?<journal>.*)\}/Uu', $bibtext, $m))
 						{
-							$reference->journal = $m['journal'];
+							$reference->{'container-title'}[] = $m['journal'];
+							$reference->type = 'journal-article';
 						}
 					}
 	
-					if ($date == '')
+					if (!isset($reference->issued))
 					{
+						$reference->issued = new stdclass;
+						$reference->issued->{'date-parts'} = array();
 						if (preg_match('/year = \{(?<year>[0-9]{4})\}/', $bibtext, $m))
 						{
-							$reference->date = $m['year'];
+							$reference->issued->{'date-parts'}[] = array($m['year']);
 						}
 					}
 			
@@ -92,20 +105,130 @@ function orcid_works($obj, $lookup_works)
 					// pages = {41-68}
 					if (preg_match('/pages = \{(?<pages>.*)\}/Uu', $bibtext, $m))
 					{
-						$pages = $m['pages'];
-						if (preg_match('/(?<spage>\d+)-[-]?(?<epage>\d+)/', $pages, $mm))
-						{
-							$reference->pageStart = $mm['spage'];
-							$reference->pageEnd = $mm['epage'];
-						}
-						else
-						{	
-							$reference->pages = $pages;
-						}
+						$reference->page = $m['pages'];
+						$reference->page = str_replace('--', '-', $reference->page);
 					}
 				}
+				
+				//------------------------------------------------------------------------
+				if (isset($work->{'work-contributors'}))
+				{		
+					$reference->author = array();
+				
+					// OK, since this person is an author, find the best matching name amongst
+					// and use the ORCID for that person. The others will be blank nodes.
+					// ORCID has a field "contributor-orcid" but this always seems to be null :(
+
+					foreach ($work->{'work-contributors'}->{'contributor'} as $contributor)
+					{
+						$author = new stdclass;
+											
+						// Parse the name
+						$parts = parse_name($contributor->{'credit-name'}->value);
 		
-				// Identifiers
+						if (isset($parts['last']))
+						{
+							$author->family = $parts['last'];
+						}
+						if (isset($parts['first']))
+						{
+							$author->given = $parts['first'];
+			
+							if (array_key_exists('middle', $parts))
+							{
+								$author->given .= ' ' . $parts['middle'];
+							}
+						}
+						
+						if (!isset($author->family) || !isset($author->given))
+						{
+							$author->literal = $contributor->{'credit-name'}->value;
+						}
+					
+						$reference->author[] = $author;
+					}
+				}
+				
+				// match to ORCID
+				// code from recon-15
+				if (isset($reference->author))
+				{
+				
+					$target = new stdclass;
+					$target->name = '';
+
+					$personal_details = $obj->{'orcid-profile'}->{'orcid-bio'}->{'personal-details'};
+
+					if (isset($personal_details->{'given-names'}))
+					{
+						$target->name = $personal_details->{'given-names'}->{'value'};
+					}
+
+					if (isset($personal_details->{'family-name'}))
+					{
+						$target->name .= $personal_details->{'family-name'}->{'value'};
+					}
+
+					if (isset($personal_details->{'credit-name'}))
+					{
+						$target->credit_name = $personal_details->{'credit-name'}->{'value'};
+					}
+
+					if (isset($personal_details->{'other-names'}))
+					{
+						foreach ($personal_details->{'other-names'}->{'other-name'} as $other_name)
+						{
+							$target->other_names[] = $other_name->value;
+						}
+					}				
+					$min_d = 100;
+					$hit = -1;
+
+					$n = count($reference->author);
+					for ($i = 0; $i < $n; $i++)
+					{
+						$name = '';
+						if (isset($reference->author[$i]->literal))
+						{
+							$name = $reference->author[$i]->literal;
+						}
+						else
+						{
+						 $name = $reference->author[$i]->given . ' ' . $reference->author[$i]->family;
+						}
+					
+					
+						if (isset($target->name))
+						{
+							$d = levenshtein(finger_print($name), finger_print($target->name));
+
+							if ($d < $min_d)
+							{
+								$min_d = $d;
+								$hit = $i;
+							}
+						}
+
+						if (isset($target->credit_name))
+						{
+							$d = levenshtein(finger_print($name), finger_print($target->credit_name));
+
+							if ($d < $min_d)
+							{
+								$min_d = $d;
+								$hit = $i;
+							}
+						}
+
+					}
+					
+					if ($hit != -1)
+					{
+						$reference->author[$hit]->ORCID = $orcid;
+					}						
+				}
+		
+				// Identifiers------------------------------------------------------------
 				if (isset($work->{'work-external-identifiers'}))
 				{
 					foreach ($work->{'work-external-identifiers'}->{'work-external-identifier'} as $identifier)
@@ -120,7 +243,7 @@ function orcid_works($obj, $lookup_works)
 								$value = preg_replace('/\s+/', '', $value);
 					
 								// DOI
-								$reference->doi = $value;
+								$reference->DOI = $value;
 								break;
 						
 							case 'ISBN':
@@ -128,7 +251,7 @@ function orcid_works($obj, $lookup_works)
 						
 								if ($work_type == 'BOOK')
 								{
-									$reference->isbn = $value;
+									$reference->isbn[] = $value;
 								}												
 								break;
 
@@ -136,17 +259,17 @@ function orcid_works($obj, $lookup_works)
 								$value = $identifier->{'work-external-identifier-id'}->value;
 								$parts = explode(";", $value);
 						
-								$reference->issn = $parts;
+								$reference->ISSN[] = $parts;
 								break;
 
 							case 'PMC':
 								$value = $identifier->{'work-external-identifier-id'}->value;
-								$reference->pmc = $value;
+								$reference->PMC = $value;
 								break;
 
 							case 'PMID':
 								$value = $identifier->{'work-external-identifier-id'}->value;
-								$reference->pmid = $value;
+								$reference->PMID = $value;
 								break;
 					
 							default:
@@ -161,10 +284,12 @@ function orcid_works($obj, $lookup_works)
 					if (isset($work->{'url'}->{'value'}))
 					{
 						$urls = explode(",", $work->{'url'}->{'value'});
-						$reference->url = $urls[0];
+						$reference->URL = $urls[0];
 					}
 				}
 				
+				/*
+				// to do: need to fix this code
 				if ($lookup_works)
 				{
 					// look for DOI if not present
@@ -193,31 +318,50 @@ function orcid_works($obj, $lookup_works)
 						}
 					}
 				}
+				*/
 
-				// keep track of these for now
-				$works[] = $reference;
-		
+				$doc->message = $reference;
+				
+				$doc->{'message-timestamp'} = date("c", time());
+				$doc->{'message-modified'} 	= $doc->{'message-timestamp'};
+				
+				//echo json_encode($doc) . "\n";
+				
+				// add to database--------------------------------------------------------
+				$exists = $couch->exists($doc->_id);
+				if (!$exists)
+				{
+					$couch->add_update_or_delete_document($doc, $doc->_id, 'add');	
+				}
+				else
+				{
+					if ($force)
+					{
+						$couch->add_update_or_delete_document($doc, $doc->_id, 'update');
+					}
+				}
+				
 				// links for this work
 				$link = '';
 				if ($link == '')
 				{
-					if (isset($reference->doi))
+					if (isset($reference->DOI))
 					{
-						$link = 'http://dx.doi.org/' . $reference->doi;
+						$link = 'http://dx.doi.org/' . $reference->DOI;
 					}
 				}
 				if ($link == '')
 				{
-					if (isset($reference->pmid))
+					if (isset($reference->PMID))
 					{
-						$link = 'http://www.ncbi.nlm.nih.gov/pubmed/' . $reference->pmid;
+						$link = 'http://www.ncbi.nlm.nih.gov/pubmed/' . $reference->PMID;
 					}		
 				}
 				if ($link == '')
 				{
-					if (isset($reference->pmc))
+					if (isset($reference->PMC))
 					{
-						$link = 'http://www.ncbi.nlm.nih.gov/pmc/articles/' . $reference->pmc;
+						$link = 'http://www.ncbi.nlm.nih.gov/pmc/articles/' . $reference->PMC;
 					}		
 				}
 
@@ -227,9 +371,9 @@ function orcid_works($obj, $lookup_works)
 				}
 		
 				// links for container
-				if (isset($reference->issn))
+				if (isset($reference->ISSN))
 				{
-					foreach ($reference->issn as $issn)
+					foreach ($reference->ISSN as $issn)
 					{				
 						$link = 'http://www.worldcat.org/issn/' . $issn;
 						if (!in_array($link, $links))
@@ -242,7 +386,6 @@ function orcid_works($obj, $lookup_works)
 			}
 		}
 	}	
-	
 	$links = array_unique($links);
 	
 	return $links;	
@@ -259,7 +402,9 @@ function orcid_fetch($orcid, $lookup_works = false)
 	$url = 'http://pub.orcid.org/v1.2/' . $orcid . '/orcid-profile';
 	$json = get($url, '', 'application/orcid+json');
 	
-//	$json = file_get_contents('0000-0002-7573-096X.json');
+	//$json = file_get_contents(dirname(__FILE__) . '/0000-0003-0566-372X.json');
+	//file_put_contents(dirname(__FILE__) . '/0000-0003-0566-372X.json', $json);
+	
 	
 	//echo $json;
 	
